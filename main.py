175ad4ac8521ef5331fcf3624156687dff6a2f6f
Dya Adel ElNahal <DyaPlus@gmail.com>
2020-05-12 22:56:47 +0200

    Updated main.py to use the MainChessAPpp

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from MainChessApp import MainChessApp
 
 if __name__ == '__main__': 
-    #Instantiate main
-    print('main')
+    #Instantiate Application
+    chessApp = MainChessApp()
+
+    #Build Application UI and show it
+    chessApp.buildUI()
