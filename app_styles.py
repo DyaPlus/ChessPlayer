45514e9d0fa04f72f3d2104872eb2415242a59a4
AbdullahHajjaj <36624498+AbdullahHajjaj@users.noreply.github.com>
2020-05-12 22:34:41 +0200

    added app_styles for the main application

---
+++ app_styles.py
@@ -0,0 +1,32 @@
+app_styles = """
+        QWidget { 
+            font-family: Century Gothic;
+            }
+        QWidget#window { 
+            background-color: white;
+            }
+        QPushButton { 
+            background-color: #0B5394; 
+            color : white; 
+            border-style: outset; 
+            border-width: 2px;
+            border-color: white ;
+            border-radius: 10px;
+            border-color: white;
+            font: bold 14px;
+            font-family: Century Gothic;
+            min-width: 10em;
+            padding: 6px;
+            }
+            QPushButton#small { 
+            background-color: #0B5394; 
+            color : white; 
+            border-style: outset; 
+            border-width: 2px;
+            font: bold 10px;
+            font-family: Century Gothic;
+            min-width: 5em;
+            padding: 2px;
+            }
+        
+            """
